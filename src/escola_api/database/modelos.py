7e5db4f21594e14d4f6909311c0e42f0384fd1ee
Sammadar dos Santos <sammadar@outlook.com.br>
2025-06-02 21:55:17 -0300

    Adicionando Banco de Dados

---
+++ src/escola_api/database/modelos.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+
+from escola_api.database.banco_dados import Base
+
+class Curso(Base):
+    __tablename__ = "curso"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String(50), nullable=False)
+    sigla = Column(String(3), nullable=False)
